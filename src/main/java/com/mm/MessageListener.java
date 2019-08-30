cdaf155b8c1d02b882f36ba89ba33c70883f5ca6
ketangote <ketan.gote@metamagic.in>
2019-08-30 22:09:36 +0530

    Changes: Updated

--- src/main/java/com/mm/MessageListener.java
+++ src/main/java/com/mm/MessageListener.java
@@ -23,6 +23,7 @@ import atg.taglib.json.util.JSONObject;
 
 /**
  * Message Listener for RabbitMQ
+ * 
  * @author deepak.af.kumar
  *
  */
@@ -32,9 +33,9 @@ public class MessageListener {
 
 	@Value("${json.path}")
 	private String responseFilePath;
-	
-    private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
-    
+
+	private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
+
 	private final RabbitTemplate rabbitTemplate;
 	private ApplicationConfigReader applicationConfig;
 	private MessageSender messageSender;
@@ -62,103 +63,105 @@ public class MessageListener {
 		this.messageSender = messageSender;
 	}
 
-    /**
-     * Message listener for app1
-     * @param UserDetails a user defined object used for deserialization of message
-     */
-    @RabbitListener(queues = "${app1.queue.name}")
-    public void receiveMessageForApp1(final Object data) {
-    	log.info("Received message: {} from app1 queue.", data);
-    	System.out.println("**************MESSAGE RECEIVED***************");
-    	System.out.println("DATA AS OBJECT "+data);
+	/**
+	 * Message listener for app1
+	 * 
+	 * @param UserDetails
+	 *            a user defined object used for deserialization of message
+	 */
+	@RabbitListener(queues = "${app1.queue.name}")
+	public void receiveMessageForApp1(final Object data) {
+		log.info("Received message: {} from app1 queue.", data);
+		System.out.println("**************MESSAGE RECEIVED***************");
+		System.out.println("DATA AS OBJECT " + data);
 		String exchange = getApplicationConfig().getApp2Exchange();
 		String routingKey = getApplicationConfig().getApp2RoutingKey();
 
 		
+		System.out.println("**************MESSAGE RECEIVED***************");
+
 		try {
 			String decodeData = this.decode(data.toString());
-			System.out.println("*******decodeData********"+decodeData);
+			System.out.println("*******decodeData********" + decodeData);
 			atg.taglib.json.util.JSONObject json = new atg.taglib.json.util.JSONObject(decodeData);
-			System.out.println("JSON DATA "+json);
+			System.out.println(" decodeData JSON DATA " + json);
+			
+				
+			
+			if (json.has("Data")) {
+				String strData = json.getString("Data");
+				System.out.println("**********strData********"+strData);
+				/*
+				if (requestMetaData.has("Command")) {
+					String command = requestMetaData.getString("Command");
+					String commandSeqId = requestMetaData.getString("commandSeqId");
+					if (command != null && commandSeqId != null) {
+						command = command.toLowerCase().replace(" ", "");
+						Object response = this.getRequestResponse(command, commandSeqId);
+						messageSender.sendMessage(rabbitTemplate, exchange, routingKey, response.toString());
+						log.info("<< Exiting receiveMessageForApp1() after API call.");
+					}
+
+				}
+				*/
+			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			log.error("Internal server error occurred in API call. Bypassing message requeue {}", e);
 			e.printStackTrace();
 		}
-		System.out.println("**************MESSAGE RECEIVED***************");
-		
-		
-//    	try {
-//    		log.info("Getting Response Associated to Request");
-//    		atg.taglib.json.util.JSONObject json = new atg.taglib.json.util.JSONObject(data);
-//    		if(json.has("Data") && json.getJSONObject("Data").has("RequestMetaData")){
-//    			JSONObject requestMetaData = json.getJSONObject("Data").getJSONObject("RequestMetaData");
-//    			if(requestMetaData.has("Command")){
-//    				String command = requestMetaData.getString("Command");
-//    				String commandSeqId = requestMetaData.getString("commandSeqId");
-//    				if (command !=null && commandSeqId!=null){
-//    					command = command.toLowerCase().replace(" ", "");
-//    					Object response = this.getRequestResponse(command, commandSeqId);
-//    		    		messageSender.sendMessage(rabbitTemplate, exchange, routingKey, response.toString());
-//    		        	log.info("<< Exiting receiveMessageForApp1() after API call.");
-//    				}
-//    				
-//    			}
-//    		}
-//    	}  catch(Exception e) {
-//    		log.error("Internal server error occurred in API call. Bypassing message requeue {}", e);
-//    		throw new AmqpRejectAndDontRequeueException(e); 
-//    	}
 
-    }
-    
-    public  String decode(String url)  
-    {  
-              try {  
-                   String prevURL="";  
-                   String decodeURL=url;  
-                   while(!prevURL.equals(decodeURL))  
-                   {  
-                        prevURL=decodeURL;  
-                        decodeURL=URLDecoder.decode( decodeURL, "UTF-8" );  
-                   }  
-                   String decodedData = decodeURL.replace("(Body:", "");
-                   decodedData = decodedData.replace("\"Data\":\"{", "\"Data\":{");
-                   decodedData = decodedData.replace("}}\"}", "}}}");
-                   return decodedData;  
-              } catch (UnsupportedEncodingException e) {  
-                   return "Issue while decoding" +e.getMessage();  
-              }  
-    }  
-	
-    public static void main(String[] args) {
+	}
+
+	public String decode(String url) {
+		try {
+			String prevURL = "";
+			String decodeURL = url;
+			while (!prevURL.equals(decodeURL)) {
+				prevURL = decodeURL;
+				decodeURL = URLDecoder.decode(decodeURL, "UTF-8");
+			}
+			String decodedData = decodeURL.replace("(Body:", "");
+			decodedData = decodedData.replace("\"Data\":\"{", "\"Data\":{");
+			decodedData = decodedData.replace("}}\"}", "}}}");
+			return decodedData;
+		} catch (UnsupportedEncodingException e) {
+			return "Issue while decoding" + e.getMessage();
+		}
+	}
+	// {"Initial":"true","Data":{\"RequestMetaData\":{\"Command\":\"Search EandI\",\"requestId\":1567182814107,\"requestTime\":1567182814098,\"commandSeqId\":1},\"RequestParams\":{\"memberID\":\"\",\"FirstName\":\"\",\"LastName\":\"\",\"DOB\":null}}}' MessageProperties [headers={}, contentType=text/plain, contentLength=0, redelivered=false, receivedExchange=, receivedRoutingKey=request, deliveryTag=1, consumerTag=amq.ctag-K6-tA-bci2RhtKqhPwm7qA, consumerQueue=request])
+
+	public static void main(String[] args) {
 		String str = args[0];
 		try {
-			String data = URLDecoder.decode( str, "UTF-8" ).replace("(Body:", "");
-			
+			String data = URLDecoder.decode(str, "UTF-8").replace("(Body:", "");
+
 			System.out.println(data);
 			data = data.replace("\"Data\":\"{", "\"Data\":{");
 			System.out.println(data);
 			data = data.replace("}}\"}", "}}}");
 			System.out.println(data);
 			System.out.println(new JSONObject(data));
+			
+			
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 	}
-    private Object getRequestResponse(String command, String commandSeqId){
-    	String fileName = command+"_"+commandSeqId+".json";
-    	
-    	String filePath = responseFilePath+fileName;
-    	System.out.println("Sending response from file ..."+filePath);
-    	StringBuilder content = new StringBuilder(); 
-    	try {
+
+	private Object getRequestResponse(String command, String commandSeqId) {
+		String fileName = command + "_" + commandSeqId + ".json";
+
+		String filePath = responseFilePath + fileName;
+		System.out.println("Sending response from file ..." + filePath);
+		StringBuilder content = new StringBuilder();
+		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
-			String line ;
-			while((line = br.readLine())!=null){
+			String line;
+			while ((line = br.readLine()) != null) {
 				content.append(line);
 			}
 			return new JSONObject(content.toString());
@@ -169,24 +172,22 @@ public class MessageListener {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-    	
-    	return "";
-    }
-    
-    
-    /**
-     * Message listener for app2
-     * 
-      Enable for testing purpose.
-    */
-	    @RabbitListener(queues = "${app2.queue.name}")
-	    public void receiveMessageForApp2(Object data) {
-	    	log.info("Received message: {} from app2 queue.", data);
-	    	System.out.println("**************MESSAGE RECEIVED***************");
-	    	System.out.println("DATA AS OBJECT2 "+data);
-	    	System.out.println("DATA AS TOSTRING2 "+data.toString());
-	    	System.out.println("**************MESSAGE RECEIVED***************");
-	    }
 
-     
+		return "";
+	}
+
+	/**
+	 * Message listener for app2
+	 * 
+	 * Enable for testing purpose.
+	 */
+	@RabbitListener(queues = "${app2.queue.name}")
+	public void receiveMessageForApp2(Object data) {
+		log.info("Received message: {} from app2 queue.", data);
+		System.out.println("**************MESSAGE RECEIVED***************");
+		System.out.println("DATA AS OBJECT2 " + data);
+		System.out.println("DATA AS TOSTRING2 " + data.toString());
+		System.out.println("**************MESSAGE RECEIVED***************");
+	}
+
 }
